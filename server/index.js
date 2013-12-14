7c409a04c1ac698f37fd212d7b3880254122e2c9
kpdecker <kpdecker@gmail.com>
2013-12-14 17:44:14 -0600

    Implement hanging service endpoint

--- server/index.js
+++ server/index.js
@@ -4,9 +4,23 @@ var server = Hapi.createServer('localhost', 8000);
 
 server.route({
   method: 'GET',
+  path: '/hang',
+  handler: function(request, reply) {
+    console.log(new Date(), 'hang');
+
+    if (request.query.duration) {
+      setTimeout(function() {
+        reply(new Hapi.response.Empty());
+      }, parseInt(request.query.duration, 10));
+    }
+  }
+});
+
+server.route({
+  method: 'GET',
   path: '/log',
   handler: function(request, reply) {
-    console.log(request.query.log);
+    console.log(new Date(), 'log', request.query.log);
 
     reply(new Hapi.response.Empty());
   }
