9f1b90efaae160c7f506c542486b727b016a063b
kpdecker <kpdecker@gmail.com>
2013-12-14 00:38:56 -0600

    Add logging endpoint

--- server/index.js
+++ server/index.js
@@ -4,6 +4,16 @@ var server = Hapi.createServer('localhost', 8000);
 
 server.route({
   method: 'GET',
+  path: '/log',
+  handler: function(request, reply) {
+    console.log(request.query.log);
+
+    reply(new Hapi.response.Empty());
+  }
+});
+
+server.route({
+  method: 'GET',
   path: '/{path*}',
   handler: {
     directory: {
