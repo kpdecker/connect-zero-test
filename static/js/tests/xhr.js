18ac385b7f4d7e9df3b39b276b45156fa39da313
kpdecker <kpdecker@gmail.com>
2013-12-16 09:42:03 -0600

    Always log all XHR events

--- static/js/tests/xhr.js
+++ static/js/tests/xhr.js
@@ -1,34 +1,30 @@
 (function() {
   var xhr = new XMLHttpRequest();
 
-  if (testOptions.all || testOptions.xhr2) {
-    xhr.onloadstart = function() {
-      logStep('loadstart', testOptions.url);
-    };
-    xhr.onprogress = function() {
-      logStep('progress', testOptions.url);
-    };
-    xhr.onabort = function() {
-      logStep('abort', testOptions.url);
-    };
-    xhr.onerror = function() {
-      logStep('error', testOptions.url);
-    };
-    xhr.onload = function() {
-      logStep('load', testOptions.url);
-    };
-    xhr.ontimeout = function() {
-      logStep('timeout', testOptions.url);
-    };
-    xhr.onloadend = function() {
-      logStep('loadend', testOptions.url);
-    };
-  }
-  if (testOptions.all || testOptions.xhr1) {
-    xhr.onreadystatechange = function(){
-      logStep('readystatechange', xhr.readyState, 'status:', xhr.readyState > 1 && xhr.status);
-    };
-  }
+  xhr.onloadstart = function() {
+    logStep('loadstart', testOptions.url);
+  };
+  xhr.onprogress = function() {
+    logStep('progress', testOptions.url);
+  };
+  xhr.onabort = function() {
+    logStep('abort', testOptions.url);
+  };
+  xhr.onerror = function() {
+    logStep('error', testOptions.url);
+  };
+  xhr.onload = function() {
+    logStep('load', testOptions.url);
+  };
+  xhr.ontimeout = function() {
+    logStep('timeout', testOptions.url);
+  };
+  xhr.onloadend = function() {
+    logStep('loadend', testOptions.url);
+  };
+  xhr.onreadystatechange = function(){
+    logStep('readystatechange', xhr.readyState, 'status:', xhr.readyState > 1 && xhr.status);
+  };
 
   logStep('xhr-request', testOptions.url);
   xhr.open('GET', testOptions.url, !testOptions.sync);
